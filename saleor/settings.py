b1f4e426a593765a20ded9954e91cd51af2095ac
Ajay kumar <ajaykumar.bit.1995@gmail.com>
2020-10-09 15:09:33 +0530

    Update settings.py

--- saleor/settings.py
+++ saleor/settings.py
@@ -47,6 +47,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 _DEFAULT_CLIENT_HOSTS = "localhost,127.0.0.1,a110125.herokuapp.com"
+ALLOWE_HOSTS = "a110125.herokuapp.com, https://a110125.herokuapp.com, http://a110125.herokuapp.com/"
 
 ALLOWED_CLIENT_HOSTS = os.environ.get("ALLOWED_CLIENT_HOSTS")
 if not ALLOWED_CLIENT_HOSTS:
